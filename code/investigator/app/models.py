03149fba352f806af6de63539fe6b4ddb5a0a0f8
Eric Schles <ericschles@gmail.com>
2016-06-10 12:18:58 -0400

    updating

--- code/investigator/app/models.py
+++ code/investigator/app/models.py
@@ -1,3 +1,11 @@
+"""
+
+Here the models for our database is defined.
+
+I am using Postgres, Flask-SQLAlchemy for this application.
+
+For an introduction to Flask-SQLAlchemy check out: http://flask-sqlalchemy.pocoo.org/2.1/
+""" 
 from app import db
 
 class BackpageAdInfo(db.Model):
