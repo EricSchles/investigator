ba867cb4b2a5b2c09a7cd4f6e941d6db7c743f57
Eric Schles <ericschles@gmail.com>
2017-04-29 05:15:18 -0400

    updating init for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
+import os
 
 username,password = "eric_s","1234"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/backpage_ads"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/backpage_ads"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
