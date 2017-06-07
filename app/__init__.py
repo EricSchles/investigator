59494bb36557ed7be8a45f2997dd8ae101b812ee
Eric Schles <ericschles@gmail.com>
2017-06-07 00:14:35 -0400

    preparing for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ import os
 
 username,password = "eric_s","1234"
 app = Flask(__name__)
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/backpage_ads"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"+username+":"+password+"@localhost/backpage_ads"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
